4258d07f07c30936175166b290c8770bb85ae799
dawidseipold <seipold.dawid@gmail.com>
2024-07-10 22:32:53 +0200

    feat: add a basic footer component

---
+++ src/components/layout/footer/footer.svelte
@@ -0,0 +1,20 @@
+<footer class="footer">
+	<div class="footer__wrapper">Footer</div>
+</footer>
+
+<style lang="scss">
+	@import '$styles/abstracts/variables';
+	@import '$styles/abstracts/mixins';
+	@import '$styles/abstracts/breakpoints';
+
+	.footer {
+		background-color: $footer-background;
+		color: $footer-foreground;
+		padding: 1rem;
+		text-align: center;
+
+		&__wrapper {
+			@include wrapper;
+		}
+	}
+</style>
---
+++ src/components/layout/footer/index.ts
@@ -0,0 +1,5 @@
+import Root from './footer.svelte';
+
+interface Props {}
+
+export { Root as Footer, type Props as FooterProps };
