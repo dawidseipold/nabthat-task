0a0d47885accbeea567414c93d6ade848a65ee11
dawidseipold <seipold.dawid@gmail.com>
2024-07-10 22:33:21 +0200

    feat: add blockone component (unfinished)

---
+++ src/components/layout/block-one/block-one.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { type BlockOneProps, type Option, OPTIONS } from '.';
+</script>
+
+{#snippet option(option: Option)}
+	<p>{option.title}</p>
+{/snippet}
+
+<div class="block">
+	<h2 class="heading">blok pierwszy</h2>
+
+	<ul>
+		{#each OPTIONS as opt (opt.title)}
+			<li>{@render option(opt)}</li>
+		{/each}
+	</ul>
+</div>
+
+<style lang="scss">
+	@import '$styles/abstracts/variables';
+	@import '$styles/abstracts/mixins';
+
+	.heading {
+		@include heading-secondary;
+	}
+
+	.block {
+		@include block;
+	}
+</style>
