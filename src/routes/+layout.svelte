c13e3f972667e9908d03c2c157937b442013fffc
dawidseipold <seipold.dawid@gmail.com>
2024-07-10 22:33:45 +0200

    feat: add a layout and main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { Footer } from '$components/layout/footer';
+	import { Header } from '$components/layout/header';
+
+	let { children } = $props();
+</script>
+
+<div class="wrapper">
+	<Header />
+	<main class="main">
+		<div class="main__wrapper">{@render children()}</div>
+	</main>
+	<Footer />
+</div>
+
+<style lang="scss">
+	@import '$styles/abstracts/variables';
+	@import '$styles/abstracts/breakpoints';
+	@import '$styles/abstracts/mixins';
+
+	.wrapper {
+		font-family: Arial, Helvetica, sans-serif;
+		min-height: 100dvh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.main {
+		background: $content-background;
+		display: flex;
+		flex: 1;
+
+		&__wrapper {
+			@include wrapper;
+
+			padding-top: 1rem;
+			padding-bottom: 1rem;
+
+			@include md {
+				padding-top: 2rem;
+				padding-bottom: 2rem;
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { BlockOne } from '$components/layout/block-one';
+</script>
+
+<div class="container">
+	<h1 class="heading">Nagłówek H1</h1>
+
+	<div class="blocks">
+		<BlockOne />
+	</div>
+</div>
+
+<style lang="scss">
+	@import '$styles/abstracts/variables';
+	@import '$styles/abstracts/mixins';
+	@import '$styles/abstracts/breakpoints';
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.heading {
+		color: $content-foreground;
+
+		@include heading-primary;
+	}
+</style>
