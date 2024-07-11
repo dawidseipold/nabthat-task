4f1d16b71c3988ad406479681552bf4b59c1b251
dawidseipold <seipold.dawid@gmail.com>
2024-07-11 23:10:12 +0200

    feat: add css-is-awesome component

---
+++ src/components/data-display/css-is-awesome/css-is-awesome.svelte
@@ -0,0 +1,36 @@
+<div class="element">
+	<span class="element__line">css</span>
+	<span class="element__line">is</span>
+	<span class="element__line">awesome</span>
+</div>
+
+<style lang="scss">
+	@import '$styles/abstracts/variables';
+
+	.element {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		justify-content: space-between;
+		padding: 8px;
+		border: 1px solid $footer-foreground;
+		height: 6rem;
+		width: 6rem;
+
+		&:hover {
+			border-color: $accent;
+			cursor: pointer;
+		}
+
+		&__line {
+			font-size: 1.25rem;
+			font-weight: 500;
+			color: $footer-foreground;
+			text-transform: uppercase;
+
+			.element:hover & {
+				color: $footer-foreground-hover;
+			}
+		}
+	}
+</style>
---
+++ src/components/data-display/css-is-awesome/index.ts
@@ -0,0 +1,5 @@
+import Root from './css-is-awesome.svelte';
+
+interface Props {}
+
+export { Root as CssIsAwesome, type Props as CssIsAwesomeProps };
