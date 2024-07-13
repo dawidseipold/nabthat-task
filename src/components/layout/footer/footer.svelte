b739f80c79d99fea43e98b621b9ea479a966a0df
dawidseipold <seipold.dawid@gmail.com>
2024-07-13 16:31:19 +0200

    refacotr: refactor the way the footer dropdown works and use meltui for its structure feat: add a chevron snippet for usage in all of the footer buttons

--- src/components/layout/footer/footer.svelte
+++ src/components/layout/footer/footer.svelte
@@ -1,18 +1,41 @@
 <script lang="ts">
 	import CssIsAwesome from '$components/data-display/css-is-awesome/css-is-awesome.svelte';
+	import { getContentState } from '$context/content-state.svelte';
 	import { getDetailsState } from '$context/details-state.svelte';
-	import type { HTMLDialogAttributes } from 'svelte/elements';
+	import { getOptionsState } from '$context/option-state.svelte';
+	import { createDropdownMenu, melt } from '@melt-ui/svelte';
 
 	const detailsState = getDetailsState();
-
 	const detailsHandler = () => {
 		detailsState.toggleVisibility();
 	};
-
 	let detailsVisible = $derived(detailsState.get()?.visible);
+	let content = getContentState();
+	let details = getDetailsState();
 
-	let dialog = $state<HTMLDialogElement>();
+	const {
+		elements: { menu, item, trigger },
+		states: { open }
+	} = createDropdownMenu({});
+
+	const resetSettings = () => {
+		content.reset();
+		details.set({ visible: false });
+	};
 </script>
+
+{#snippet chevron(rotation: number = 0)}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		class="chevron"
+		style="--rotate: {rotation}deg;"
+	>
+		<path fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6z" />
+	</svg>
+{/snippet}
 
 <footer class="footer">
 	<div class="footer__wrapper">
@@ -21,31 +44,47 @@
 		<div class="footer__logo">nabthat</div>
 
 		<div class="footer__options">
-			<button onclick={() => dialog && dialog.show()} class="footer__button"
-				>Open Modal dialog</button
-			>
+			<button use:melt={$trigger} class="footer__options__button">
+				pokaż
+				{@render chevron($open ? 180 : 0)}
+			</button>
 
-			<dialog bind:this={dialog} class="footer__dialog">
-				<button class="footer__dialog__button"> zresetuj ustawienia </button>
-
-				<button
-					class="footer__dialog__button"
-					onclick={() => {
-						detailsHandler();
-					}}
-				>
-					{detailsVisible ? 'schowaj' : 'pokaż'} dane osobowe
-				</button>
-				<button onclick={() => dialog && dialog.close()}>Close</button>
-			</dialog>
+			{#if $open}
+				<div use:melt={$menu} class="footer__options__dropdown">
+					<button
+						use:melt={$item}
+						class="footer__options__dropdown__button"
+						onclick={() => {
+							resetSettings();
+						}}
+					>
+						{@render chevron(90)}
+						zresetuj ustawienia
+					</button>
+					<button
+						use:melt={$item}
+						class="footer__options__dropdown__button"
+						onclick={() => {
+							detailsHandler();
+						}}
+					>
+						{@render chevron(90)}
+						{detailsVisible ? 'schowaj' : 'pokaż'} dane osobowe
+					</button>
+				</div>
+			{/if}
 		</div>
 	</div>
 </footer>
 
 <style lang="scss">
-	@import '$styles/abstracts/variables';
-	@import '$styles/abstracts/mixins';
-	@import '$styles/abstracts/breakpoints';
+	@import '$styles/variables';
+	@import '$styles/mixins';
+	@import '$styles/breakpoints';
+
+	.chevron {
+		rotate: var(--rotate, 0deg);
+	}
 
 	.footer {
 		background-color: $footer-background;
@@ -69,7 +108,9 @@
 			display: none;
 
 			@include breakpoint('md') {
-				display: flex;
+				& {
+					display: flex;
+				}
 			}
 
 			&::before,
@@ -86,6 +127,51 @@
 
 			&::after {
 				margin-left: 0.25rem;
+			}
+		}
+
+		&__options {
+			&__button {
+				color: $dimmed-foreground;
+				border: 1px solid $footer-foreground;
+				border-radius: 4px;
+				padding: 0.75rem 4rem;
+				text-transform: uppercase;
+				font-weight: 700;
+				display: flex;
+				column-gap: 0.25rem;
+				align-items: center;
+
+				&:hover {
+					color: $content-foreground;
+					cursor: pointer;
+				}
+			}
+
+			&__dropdown {
+				background-color: $content-foreground;
+				display: flex;
+				flex-direction: column;
+				justify-content: start;
+				row-gap: 1rem;
+				padding: 1rem;
+				border-radius: 0.25rem;
+				box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+
+				&__button {
+					display: flex;
+					column-gap: 0.25rem;
+					align-items: center;
+					text-transform: uppercase;
+					font-weight: 600;
+					color: $header-background;
+					font-family: Arial, Helvetica, sans-serif;
+
+					&:hover {
+						color: $accent;
+						cursor: pointer;
+					}
+				}
 			}
 		}
 	}
