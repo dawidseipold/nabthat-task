be966e2633a533d85f121277643f89123ff58d36
dawidseipold <seipold.dawid@gmail.com>
2024-07-13 16:31:44 +0200

    refactor: huge refactor and simplification of the main content of the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,65 @@
 <script lang="ts">
 	import { Button } from '$components/data-display/button';
 	import { Block } from '$components/layout/block';
-	import { DATA, OPTIONS, type Option } from '$utils/constants';
+	import { getContentState } from '$context/content-state.svelte';
+	import { getOptionsState } from '$context/option-state.svelte';
+	import { OPTIONS } from '$utils/constants';
 
-	let data = $state(DATA);
-	let content = $state([] as string[]);
-	let selectedOption = $state(null as Option | null);
-	let selectedMethod = $state(null as 'replace' | 'add' | null);
+	let content = getContentState();
+	let options = getOptionsState();
 
-	let unusedContentIds = $state([...new Set(data.map((item) => Number(item.id)))]);
+	const alertNoOption = () => {
+		if (!options.getSelected()) {
+			alert('Wybierz opcję');
+			return true;
+		}
 
-	const resetUnusedContentIds = () => {
-		unusedContentIds = [...new Set(data.map((item) => Number(item.id)))];
+		return false;
 	};
 
-	const removeIdFromUnusedContentIds = (id: number) => {
-		unusedContentIds = unusedContentIds.filter((item) => item !== id);
+	const alertElementAdded = (contentId: number) => {
+		if (content.getUsed().filter((item) => item.id === contentId).length > 0) {
+			alert('Element został już dodany');
+			return true;
+		}
+
+		return false;
 	};
 
-	const handleSubmit = (e: Event) => {
-		e.preventDefault();
+	const alertNoUnused = () => {
+		if (content.getUnused().length === 0) {
+			alert('Brak elementów do dodania');
+			return true;
+		}
 
-		const item = selectedOption?.fn(data, unusedContentIds);
+		return false;
+	};
 
-		if (item) {
-			if (!unusedContentIds.includes(item.id)) {
-				alert('Ten element został już wykorzystany');
-				return;
-			}
+	const handleReplace = () => {
+		if (alertNoOption()) return;
 
-			if (selectedMethod === 'replace') {
-				resetUnusedContentIds();
-				removeIdFromUnusedContentIds(item.id);
+		const contentId = options.getSelected()?.fn(content.get().map((item) => item.id));
 
-				content = [item.content];
-			} else {
-				removeIdFromUnusedContentIds(item.id);
+		if (contentId) {
+			content.replace(contentId);
 
-				content = [...content, item.content];
-			}
+			return;
+		}
+	};
+
+	const handleAdd = () => {
+		if (alertNoOption()) return;
+
+		const contentId = options.getSelected()?.fn(content.getUnused().map((item) => item.id));
+
+		if (alertNoUnused()) return;
+
+		if (contentId) {
+			if (alertElementAdded(contentId)) return;
+
+			content.add(contentId);
+
+			return;
 		}
 	};
 </script>
@@ -50,49 +71,68 @@
 <div class="container">
 	<h1 class="heading">Nagłówek H1</h1>
 
-	<form onsubmit={handleSubmit} class="blocks">
-		<Block title="blok pierwszy">
-			<ul>
+	<div class="section">
+		<form class="form">
+			<Block title="blok pierwszy" class="form__radio">
 				{#each OPTIONS as opt (opt.title)}
 					{@const id = `option-${opt.id}`}
 
-					<li>
+					<div class="radio">
 						<input
+							class="radio__input"
 							type="radio"
 							{id}
 							name="option"
-							value={opt}
-							bind:group={selectedOption}
+							value={opt.id}
 							required
+							onchange={() => options.select(opt.id)}
 						/>
-						<label for={id}>{opt.title}</label>
+						<label class="radio__label" for={id}>{opt.title}</label>
+					</div>
+				{/each}
+			</Block>
+
+			<Block title="blok drugi" class="form__buttons">
+				<div>
+					<Button
+						type="submit"
+						onclick={() => {
+							handleReplace();
+						}}>Zastąp</Button
+					>
+
+					<Button
+						type="submit"
+						onclick={() => {
+							handleAdd();
+						}}>Doklej</Button
+					>
+				</div>
+			</Block>
+		</form>
+
+		<Block title="blok trzeci" class="content">
+			<ul>
+				{#each content.getUsed() as item (item)}
+					<li>
+						{@render dataPart(item.content)}
 					</li>
 				{/each}
 			</ul>
 		</Block>
-
-		<Block title="blok drugi">
-			<Button type="submit" onclick={() => (selectedMethod = 'replace')}>Zastąp</Button>
-			<Button type="submit" onclick={() => (selectedMethod = 'add')}>Doklej</Button>
-		</Block>
-
-		<Block title="blok trzeci">
-			{#each content as item (item)}
-				{@render dataPart(item)}
-			{/each}
-		</Block>
-	</form>
+	</div>
 </div>
 
 <style lang="scss">
-	@import '$styles/abstracts/variables';
-	@import '$styles/abstracts/mixins';
-	@import '$styles/abstracts/breakpoints';
+	@import '$styles/variables';
+	@import '$styles/mixins';
+	@import '$styles/breakpoints';
 
 	.container {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		row-gap: 2rem;
 		justify-content: center;
 	}
 
@@ -102,15 +142,130 @@
 		@include heading-primary;
 	}
 
-	.blocks {
-		width: 100%;
+	.section {
 		display: flex;
-		flex-wrap: wrap;
+		flex-direction: column;
+		align-items: start;
+		row-gap: 2rem;
+		width: 100%;
+
+		@include breakpoint('lg') {
+			& {
+				display: grid;
+				grid-template-columns: repeat(3, 1fr);
+				column-gap: 2rem;
+			}
+		}
+	}
+
+	.form {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-auto-rows: auto;
+
 		justify-content: space-between;
-		gap: 4rem;
+		column-gap: 2rem;
+		row-gap: 2rem;
+		align-items: start;
+
+		@include breakpoint('lg') {
+			& {
+				grid-column: span 2;
+			}
+		}
+
+		.radio {
+			&__input {
+				display: none;
+
+				&:checked {
+					& + label {
+						font-weight: 700;
+					}
+
+					& + .radio__label::before {
+						background-color: $content-foreground;
+						border: 2px solid white;
+						box-shadow: inset 0 0 0 4px #2e2e2e;
+					}
+				}
+			}
+
+			&__label {
+				cursor: pointer;
+				display: flex;
+				align-items: center;
+				column-gap: 0.5rem;
+
+				color: $content-foreground;
+
+				&:hover {
+					color: $dimmed-foreground;
+
+					&::before {
+						border-color: $dimmed-foreground;
+					}
+				}
+
+				&::before {
+					content: '';
+					display: inline-block;
+					width: 1.5rem;
+					height: 1.5rem;
+					border: 2px solid $content-foreground;
+					border-radius: 50%;
+				}
+			}
+		}
+
+		:global(.block.form__buttons) {
+			align-items: end;
+
+			@include breakpoint('lg') {
+				& {
+					align-items: center;
+				}
+			}
+		}
+
+		:global(.block.form__buttons div) {
+			display: flex;
+			flex-direction: column;
+			row-gap: 1rem;
+			align-items: end;
+			justify-content: center;
+
+			@include breakpoint('lg') {
+				& {
+					align-items: center;
+					flex-direction: row;
+					justify-content: space-between;
+					column-gap: 3rem;
+				}
+			}
+		}
+	}
+
+	:global(.content) {
+		grid-column: span 2;
+		width: 100%;
+	}
+
+	:global(.content ul) {
+		display: flex;
+		flex-direction: column;
+		row-gap: 0.5rem;
 		align-items: center;
 
-		@include breakpoint('md') {
+		@include breakpoint('lg') {
+			& {
+				align-items: start;
+			}
 		}
+	}
+
+	:global(.content ul li) {
+		color: $dimmed-foreground;
 	}
 </style>
