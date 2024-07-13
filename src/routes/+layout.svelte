e650d0ffa030a6e9a146e802d01051df54e2cb12
dawidseipold <seipold.dawid@gmail.com>
2024-07-13 16:30:02 +0200

    chore: initialize contexts in the layout chore: change the styles of the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
 	import { Footer } from '$components/layout/footer';
 	import { Header } from '$components/layout/header';
+	import { setContentState } from '$context/content-state.svelte';
 	import { setDetailsState } from '$context/details-state.svelte';
+	import { setOptionsState } from '$context/option-state.svelte';
+	import { CONTENT, OPTIONS } from '$utils/constants';
 
 	let { children } = $props();
 
+	// TODO: Create a function to set all states at once
 	setDetailsState();
+	setContentState(CONTENT);
+	setOptionsState(OPTIONS);
 </script>
 
 <div class="wrapper">
@@ -17,9 +23,9 @@
 </div>
 
 <style lang="scss">
-	@import '$styles/abstracts/variables';
-	@import '$styles/abstracts/breakpoints';
-	@import '$styles/abstracts/mixins';
+	@import '$styles/variables';
+	@import '$styles/breakpoints';
+	@import '$styles/mixins';
 
 	.wrapper {
 		font-family: Arial, Helvetica, sans-serif;
@@ -27,6 +33,7 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
+		overflow: hidden;
 	}
 
 	.main {
@@ -40,9 +47,11 @@
 			padding-top: 1rem;
 			padding-bottom: 1rem;
 
-			@include md {
-				padding-top: 2rem;
-				padding-bottom: 2rem;
+			@include breakpoint('md') {
+				& {
+					padding-top: 2rem;
+					padding-bottom: 2rem;
+				}
 			}
 		}
 	}
